62597f112945711c30512f8efab250f6f3a0f4b3
KaiyoteSoft <Spikeyhedgehog141@gmail.com>
2015-07-31 10:51:19 -0700

    Finished program. Added multiple style changes and included search by machine name

--- parser.js
+++ parser.js
@@ -1,90 +1,65 @@
 requiredlengthIp = 11;
-requiredlengthHw = 4;
-
-// function inputHardware() {
-//    // Searches by Hardware Address
-//    	var hwAddress = document.hwForm.hwType.value;
-//    	var i = 0;
-//    	for (i; i < jsondata.length; i++) {
-//    		if (jsondata[i].hw==hwAddress) {
-//    			var ip_data = jsondata[i].ipstr;
-//    			var count = jsondata[i].refcnt;
-//    			var hw = hwAddress;
-//    			var machineName = jsondata[i].fqname;
-//    		}
-//    	};
+requiredlengthHw = 11;
 
 
-//    	var tableData2 = "<h2>Hardware search result</h2><table> <tr>" + 
-// 	 "<th>Machine Name</th> <th>Ip Address</th>" + 
-// 	 "<th>Hardware Address</th> <th>Count</th> </tr>" +
-// 	 "<tr> <td>" + machineName + "</td> <td>" + ip_data +  "</td>" + 
-// 	 "<td>" + hw + "</td> <td>" + count + "</td> </tr>" + 
-// 	 "<tr> <td>" + machineName + "</td> <td>" + ip_data +  "</td>" + 
-// 	 "<td>" + hw + "</td> <td>" + count + "</td> </tr>" + 
-// 	 " </table>";
-// 	 console.log(tableData2);
-// 	 $('#compData2').html(tableData2);
-// }
+function inputName() {
+  var nameAddress =document.nameForm.nameType.value;
+  listContainer = [];
+  var i = 0;
+  for (i; i < jsondata.length; i++){
+    var jsonData = jsondata[i].fqname;
+    if (jsonData.startsWith(nameAddress)) {
+      var ipAddress = jsondata[i].ipstr;
+      var count = jsondata[i].refcnt;
+      var hardware = jsondata[i].hw;
+      var name = jsondata[i].fqname
+      listContainer.push("<tr> <td>" + name + "</td> <td>" + "<a onclick=\"searchIp('" +
+      ipAddress + "');\">" + ipAddress + "</a> </td>" + "<td>" +
+      "<a onclick=\"searchHardware('" + hardware + "');\">" 
+      + hardware + "</a>" + "</td>" +
+      "<td>" + count + "</td>" + "</tr>");
+    }
+  }
+  listTable = "<table> <tr> <th>Machine Name</th> <th>Ip Address</th>" + 
+  "<th>Hardware</th> <th>Count</th>" + 
+  "</tr>" + listContainer + "</table>"
 
-// function inputIp() {
-//   // Searches by ip Address
-//      var num1 = document.myform.ipType.value;
-//      var ip_data = num1;
-//      var i = 0;
-//      for (i; i < jsondata.length; i++){
-//      	if (jsondata[i].ipstr==ip_data){
-//      		var hw = jsondata[i].hw;
-//      		var count = jsondata[i].refcnt;
-//      		var machineName = jsondata[i].fqname;
-//      	}
-//      };  
-
-//      var tableData = "<h2>Ip search result </h2> <table> <tr>" + 
-// 	 "<th>Machine Name</th> <th>Ip Address</th>" + 
-// 	 "<th>Hardware Address</th> <th>Count</th> </tr>" +
-// 	 "<tr> <td>" + machineName + "</td> <td>" + ip_data +  "</td>" + 
-// 	 "<td>" + hw + "</td> <td>" + count + "</td> </tr>" + 
-// 	 "<tr> <td>" + machineName + "</td> <td>" + ip_data +  "</td>" + 
-// 	 "<td>" + hw + "</td> <td>" + count + "</td> </tr>" +
-// 	 " </table>";
-
-// 	$('#compData1').html(tableData);   
-//   }
+  $('#listData').html(listTable);
+}
 
 // Looking for all the Addresses and storing in a list
-  function inputList() {
-    var listAddress = document.listForm.listType.value;
+function inputList() {
+  var listAddress = document.listForm.listType.value;
 
-    listContainer = []
-    var i = 0;
-    for (i; i < jsondata.length; i++) {
-      var jsonData = jsondata[i].ipstr
-      if (listAddress.length >= requiredlengthIp) {
-        if (jsonData.startsWith(listAddress)) {
-          var ipAddress = jsondata[i].ipstr;
-          var count = jsondata[i].refcnt;
-          var hardware = jsondata[i].hw;
-          var name = jsondata[i].fqname;
-          listContainer.push("<tr> <td>" + name + "</td> <td>" + "<a onclick=\"searchIp('" +
-          ipAddress + "');\">" + ipAddress + "</a> </td>" + "<td>" +
-          "<a onclick=\"searchHardware('" + hardware + "');\">" 
-          + hardware + "</a>" + "</td>" +
-          "<td>" + count + "</td>" + "</tr>");
-          }
+  listContainer = []
+  var i = 0;
+  for (i; i < jsondata.length; i++) {
+    var jsonData = jsondata[i].ipstr
+    if (listAddress.length >= requiredlengthIp) {
+      if (jsonData.startsWith(listAddress)) {
+        var ipAddress = jsondata[i].ipstr;
+        var count = jsondata[i].refcnt;
+        var hardware = jsondata[i].hw;
+        var name = jsondata[i].fqname;
+        listContainer.push("<tr> <td>" + name + "</td> <td>" + "<a onclick=\"searchIp('" +
+        ipAddress + "');\">" + ipAddress + "</a> </td>" + "<td>" +
+        "<a onclick=\"searchHardware('" + hardware + "');\">" 
+        + hardware + "</a>" + "</td>" +
+        "<td>" + count + "</td>" + "</tr>");
         }
       }
-      
-      if (listAddress.length <= requiredlengthIp) {
-        listContainer.push("<tr> <td>Needs at least 11 characters for a valid search</td> </tr>");
     }
-
-    listTable = "<table> <tr> <td>Machine Name</td> <td>Ip Address</td>" + 
-    "<td>Hardware</td> <td>Count</td>" + 
-    "</tr>" + listContainer + "</table>"
-
-    $('#listData').html(listTable);
+    
+    if (listAddress.length <= requiredlengthIp) {
+      listContainer.push("<tr> <td>Needs at least 11 characters for a valid search</td> </tr>");
   }
+
+  listTable = "<table> <tr> <th>Machine Name</th> <th>Ip Address</th>" + 
+  "<th>Hardware</th> <th>Count</th>" + 
+  "</tr>" + listContainer + "</table>"
+
+  $('#listData').html(listTable);
+}
 
 function inputHardware() {
   var hwAddress = document.hwForm.hwType.value;
@@ -108,11 +83,12 @@ function inputHardware() {
     }
     
     if (hwAddress.length <= requiredlengthHw) {
-      listContainer.push("<tr> <td>Needs at least 11 characters for a valid search</td> </tr>");
+      listContainer = [];
+      listContainer.push("<tr> <td>Needs at least 12 characters for a valid search</td> </tr>");
   }
 
-  listTable = "<table> <tr> <td>Machine Name</td> <td>Hardware</td>" + 
-  "<td>Ip Address</td> <td>Count</td>" + 
+  listTable = "<table> <tr> <th>Machine Name</th> <th>Hardware</th>" + 
+  "<th>Ip Address</th> <th>Count</th>" + 
   "</tr>" + listContainer + "</table>"
 
   $('#listData').html(listTable);
@@ -136,8 +112,8 @@ function searchHardware(hw) {
         }
       }
     }
-  listTable = "<table> <tr> <td>Machine Name</td> <td>Hardware</td>" +
-  "<td>Ip Address</td> <td>Count</td>" + 
+  listTable = "<table> <tr> <th>Machine Name</th> <th>Hardware</th>" +
+  "<th>Ip Address</th> <th>Count</th>" + 
   "</tr>" + listContainer + "</table>"
   $('#listData').html(listTable);
 }
@@ -162,8 +138,8 @@ function searchIp(Ip) {
         }
       }
     }
-  listTable = "<table> <tr> <td>Machine Name</td>" +
-  "<td>Ip Address</td> <td>Hardware</td> <td>Count</td>" + 
+  listTable = "<table> <tr> <th>Machine Name</th>" +
+  "<th>Ip Address</th> <th>Hardware</th> <th>Count</th>" + 
   "</tr>" + listContainer + "</table>"
   $('#listData').html(listTable);
 }
